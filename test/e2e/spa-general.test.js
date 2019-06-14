b2c898eced2f28ac791db608f90153ce1643b13a
Fredrik Skogberg <fredrik.skogberg@medieinstitutet.se>
2019-06-14 15:51:35 +0200

    e2e test for selecting animal, erik.malsater@medieinstitutet.se

--- test/e2e/spa-general.test.js
+++ test/e2e/spa-general.test.js
@@ -5,19 +5,9 @@ import server from '../../app';
 let listeningServer;
 let driver;
 
-const PORT = 8081;
+const PORT = 8088;
 const baseUrl = `localhost:${PORT}/`;
-const timeout = 1000;
-
-const waitUntilLoaded = (element) => {
-  return driver.wait(element.getAttribute('data-loaded'))
-    .then((loaded) => {
-      if (loaded === 'true') {
-        return element;
-      }
-      return false;
-    });
-};
+const timeout = 30000;
 
 describe('html tests', () => {
   beforeAll((done) => {
@@ -37,7 +27,7 @@ describe('html tests', () => {
   test('smoke test', (done) => {
     // Find an element that is available in the static html of the page
     driver.wait(until.elementLocated(By.id('animal-listing')), timeout)
-      .then((element) => element.getAttribute('id'))
+      .then(element => element.getAttribute('id'))
       // Get its id and check that it is identical to the id we started with
       // (yes, it's a pretty stupid test - it just validates that the server and
       // selenium are working)
@@ -49,55 +39,42 @@ describe('html tests', () => {
 
   test('populate select', (done) => {
     // First find the select-tag and open it
-    driver.wait(until.elementLocated(By.id('animal-select')), timeout)
+    driver.wait(until.elementLocated(By.id('type-select')), timeout)
       .then((select) => {
         driver.wait(until.elementIsVisible(select));
         select.click();
         return select;
       })
       // List the options and simulate a click on the second item
-      .then((select) => select.findElements(By.tagName('option')))
+      .then(select => select.findElements(By.tagName('option')))
       .then((options) => {
-        driver.wait(until.elementIsVisible(options[1]));
-        options[1].click();
+        options[2].click();
       })
+      .then(() => driver.wait(until.elementLocated(By.id('animal-select')), timeout))
+      .then(element => {
+        if (!element) {
+          throw new Error('animal-select not found');
+        }
+        return element;
+      })
+      .then((select) => {
+        driver.wait(until.elementIsVisible(select));
+        select.click();
+        return select;
+      })
+      // List the options and simulate a click on the second item
+      .then(select => select.findElements(By.tagName('option')))
+      .then((options) => {
+        driver.wait(until.elementIsVisible(options[2]));
+        options[2].click();
+      })
+
       // Wait for the animal description to update
       .then(() => driver.wait(until.elementLocated(By.id('animal-description')), timeout))
-      .then(waitUntilLoaded)
       // Get the animal description text and validate it
-      .then((description) => description.getText())
+      .then(description => description.getText())
       .then((text) => {
         expect(text.length).toBeGreaterThan(0);
-        done();
-      });
-  });
-
-  test('upload animal', (done) => {
-    // Find the animal input field and fill it with animal data
-    driver.wait(until.elementLocated(By.id('animal-to-add')), timeout)
-      .then((textarea) => {
-        textarea.sendKeys('\t');
-        textarea.clear();
-        textarea.sendKeys('{"name":"test"}');
-      })
-      // Find the submit button and simulate a click
-      .then(() => driver.wait(until.elementLocated(By.id('animal-add')), timeout))
-      .then((button) => {
-        button.click();
-        return button;
-      })
-      // Wait until the load (post) on the button has finished
-      .then(waitUntilLoaded)
-      // Find the select and wait until it has updated
-      .then(() => driver.wait(until.elementLocated(By.id('animal-select')), timeout))
-      .then(waitUntilLoaded)
-      // List the options and get the last one (hopefully the one we just added)
-      .then((select) => select.findElements(By.tagName('option')))
-      .then((options) => options.pop())
-      // Validate that the text is the one we entered as a name
-      .then((lastOption) => lastOption.getText())
-      .then((text) => {
-        expect(text).toBe('test');
         done();
       });
   });
