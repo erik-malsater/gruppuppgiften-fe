b0d84b1729ee014c377ecef8ad550f38dd9279c2
Ulrica Bonnerstig <ulrica.bonnerstig@medieinstitutet.se>
2019-06-14 15:58:37 +0200

    Add validation with and without regex for animal-data form, lukas.lennstring@medieinstitutet.se

--- src/js/animalApp.js
+++ src/js/animalApp.js
@@ -10,6 +10,7 @@
   const $animalDescription = document.getElementById('animal-description');
   const $animalAdd = document.getElementById('animal-add');
   const $myForm = document.getElementById('animal-data');
+  const $error = document.getElementById('error');
 
   function clearElement(element) {
     while (element.firstChild) {
@@ -79,34 +80,68 @@
     });
   }
 
-  function listenToAdd() {
+  function validation() {
     $animalAdd.addEventListener('click', (e) => {
       e.preventDefault();
       $animalAdd.setAttribute('data-loaded', 'false');
-      const type = $myForm[0].value;
-      const dataObject = {
-        name: $myForm[1].value,
-        color: $myForm[2].value,
-        age: $myForm[3].value,
-      };
-      clearForm($myForm);
-      fetch(`http://${remoteUrl}/${type}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(dataObject),
-      })
-        .then(() => {
-          $animalAdd.setAttribute('data-loaded', 'true');
-        });
+
+      const textRegex =  /^[a-z '-]+$/i;
+      const numRegex =  /\d/g;
+      let formErrors = [];
+
+      if($myForm[0].value === ""){
+        formErrors.push($myForm[0].id);
+      }
+
+      if(!$myForm[1].value.match(textRegex)){
+        formErrors.push($myForm[1].id);
+      }
+
+      if(!$myForm[2].value.match(textRegex)){
+        formErrors.push($myForm[2].id);
+      }
+
+      if(!$myForm[3].value.match(numRegex)){
+        formErrors.push($myForm[3].id);
+      }
+
+      if(formErrors.length === 0){
+        let type = $myForm[0].value;
+        let dataObject = {
+          name: $myForm[1].value,
+          color: $myForm[2].value,
+          age: $myForm[3].value,
+        };
+        $error.innerHTML = "";
+        addAnimal(type, dataObject);
+      }else{
+        let formattedErrors = "";
+        for(let i = 0; i < formErrors.length; i++){
+          formattedErrors += formErrors[i]+"<br>";
+        }
+        $error.innerHTML = "The following fields are incorrect: <br>" + formattedErrors;
+      }
     });
+  }
+
+  function addAnimal(type, dataObject) {
+    clearForm($myForm);
+    fetch(`http://${remoteUrl}/${type}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dataObject),
+    })
+      .then(() => {
+        $animalAdd.setAttribute('data-loaded', 'true');
+      });
   }
 
   function pageLoaded() {
     listenToSelect();
     listenToType();
-    listenToAdd();
+    validation();
   }
 
   window.pageLoaded = pageLoaded;
